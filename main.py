81f5161078221fa38264f78e87f63918ba58e564
maja <maja@Alice>
2012-12-17 12:13:55 +0100

    main marged

--- main.py
+++ main.py
@@ -264,11 +264,8 @@ if __name__=='__main__':
     
     if update == 1:
         #for nex in [15]:
-<<<<<<< HEAD
         for nex in range(0,14): #range(len(all) - 2, len(all)): #[5]: #range(12, len(all)):
-=======
-        for nex in range(len(all)): #range(len(all) - 2, len(all)): #[5]: #range(12, len(all)):
->>>>>>> refs/remotes/origin/master
+
         #for nex in [15, 17]: #range(1, 15):
             filename, save_folder, intra  = find_folders(all[nex][0], all[nex][1], all[nex][2])
             #import pdb; pdb.set_trace()
